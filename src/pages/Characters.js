517225586e79a816185d44f675efc43009c6360d
Ulises <ukisessg@gmail.com>
2020-08-06 03:14:24 -0500

    Creado el primer template, la página de los personajes y está configurado webpack

---
+++ src/pages/Characters.js
@@ -0,0 +1,20 @@
+const Characters = () => {
+  const view = `
+    <div class="Characters-inner">
+      <article class="Characters-card">
+        <img src="image" alt="name">
+        <h2>Name</h2>
+      </article>
+      <article class="Characters-card">
+        <h3>Episodes:</h3>
+        <h3>Status:</h3>
+        <h3>Specie:</h3>
+        <h3>Gender:</h3>
+        <h3>Origin:</h3>
+        <h3>Last location:</h3>
+      </article>
+    </div>
+  `;
+  return view;
+};
+export default Characters;
